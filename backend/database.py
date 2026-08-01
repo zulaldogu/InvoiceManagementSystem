b8c8e64a45ee593f142e4e81bf76495c99d36bed
zulaldogu <zulalbeyzadogu@gmail.com>
2026-08-01 16:58:26 +0300

    Add Docker support for backend

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./invoice_management.db"
-
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./invoice_management.db",
+)
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
